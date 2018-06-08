d61a8a6148571aaa64d7155eca3a7dee5a1e4743
konstantinGulish <konstantin_gulish@yahoo.com>
2018-06-08 09:47:40 -0400

    Shazam Version of  FizzBuzz

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,7 +1,7 @@
 // This is a basic FizzBuzz program that prints out a list of integer adding pre-defined words to some of them
 // and testing for unique conditions
 
-
+import java.util.Random;
 public class FizzBuzz {
 
     public static void main(String[] args) {
@@ -11,8 +11,19 @@ public class FizzBuzz {
 
     // The method does not accept any arguments, and returns a string representation of the list
     private static void printNumberList() {
+        int first = 1;
+        int last = 100;
+        Random rnd = new Random();
         for (int i=1; i <=100; i++){
-            if (i%3 == 0 && i%5 == 0) {
+            if (i==1)  {
+                first = 10 + rnd.nextInt (100); // generates the first random number for the list
+                System.out.println(first);
+            }
+            if (i==100)  {
+                last = 1 + rnd.nextInt (100); // generates the last random number for the list
+                System.out.println(last);
+            }
+            else if (i%3 == 0 && i%5 == 0) {
                 System.out.println("Fizz " + " Buzz");
             }
             else if (i%3 == 0) {
@@ -24,6 +35,9 @@ public class FizzBuzz {
             else {
                 System.out.println(i);
             }
+        }
+        if (first == last){
+            System.out.println ("Shazam!!"); // prints if the first and last random numbers are the same
         }
     }
 }
